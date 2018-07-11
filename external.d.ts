3b2461f258f60873f61b969f1161e42b934638e4
wwalpha <wwalpha@gmail.com>
2018-07-11 12:22:19 +0900

    Create external.d.ts

---
+++ external.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/aws-sdk/index.d.ts" />
+/// <reference path="../node_modules/graphql-tag/index.d.ts" />
+/// <reference path="../node_modules/aws-appsync/lib/index.d.ts" />
+/// <reference path="../node_modules/apollo-client/index.d.ts" />
