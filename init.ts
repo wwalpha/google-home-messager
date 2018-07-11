ea13bdc88ac92f7d87e47012255c7aca6c198d1c
wwalpha <wwalpha@gmail.com>
2018-07-11 11:36:21 +0900

    Update init.ts

--- init.ts
+++ init.ts
@@ -1,25 +1,25 @@
 (global as any).WebSocket = require('ws');
 
 (global as any).window = (global as any).window || {
-  setTimeout: setTimeout,
-  clearTimeout: clearTimeout,
+  setTimeout,
+  clearTimeout,
   WebSocket: (global as any).WebSocket,
   ArrayBuffer: global.ArrayBuffer,
-  addEventListener: function () { },
-  navigator: { onLine: true }
+  addEventListener: () => { },
+  navigator: { onLine: true },
 };
 
 (global as any).localStorage = {
   store: {},
-  getItem: function (key: string) {
-    return this.store[key]
+  getItem: (key: string) => {
+    return this.store[key];
   },
-  setItem: function (key: string, value: any) {
-    this.store[key] = value
+  setItem: (key: string, value: any) => {
+    this.store[key] = value;
   },
-  removeItem: function (key: string) {
-    delete this.store[key]
-  }
+  removeItem: (key: string) => {
+    delete this.store[key];
+  },
 };
 
 require('es6-promise').polyfill();
