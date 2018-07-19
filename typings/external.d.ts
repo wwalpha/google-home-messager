d23f919b0b483e900cf8e7a3c99c52688fe451fb
wwalpha <wwalpha@gmail.com>
2018-07-20 00:10:29 +0900

    remove library

--- typings/external.d.ts
+++ typings/external.d.ts
@@ -1,5 +1,1 @@
 /// <reference path="../node_modules/aws-sdk/index.d.ts" />
-/// <reference path="../node_modules/aws-appsync/lib/index.d.ts" />
-/// <reference path="../node_modules/graphql-tag/index.d.ts" />
-/// <reference path="../node_modules/apollo-client/index.d.ts" />
-/// <reference path="../node_modules/apollo-cache/lib/index.d.ts" />
